9bf6d960a514c486e39fef7f43bf102884e59ce6
Aliaksandr Zahatski <zag@cpan.org>
2022-02-26 19:56:37 +0300

    fix index component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,39 @@
+import { PodNode } from "@podlite/schema"
 import Head from "next/head"
 import { DataFeedContent } from "../../bin/makeDataSource"
 import styles from "../styles/Home.module.css"
-import { getData } from "../utils"
+import { getData, getPostComponent } from "../utils"
 import { writeRss } from "../utils/rss"
 import { generateSitemap } from "../utils/sitemap"
 
-const Home = ({url, author}:IndexProps) => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Zag.ru</title>
-        <meta name="description" content="Personal web home" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to personal site of <a href={url}>{author?.name}</a>!
-        </h1>
-
-        <p className={styles.description}>
-          New version of this site is coming soon...{" "}
-        </p>
-
-        <div className={styles.grid}>
-          <a href={author?.contacts.linkedin} className={styles.card}>
-            <h2>Contact me &rarr;</h2>
-            <p>More information about me</p>
-          </a>
-
-          <a href={author?.contacts.github} className={styles.card}>
-            <h2>Code&rarr;</h2>
-            <p>Check my current projects</p>
-          </a>
-
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://pod6.in/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "} podlite
-          {/* <span className={styles.logo}>
-             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> 
-          </span> */}
-        </a>
-      </footer>
-    </div>
-  )
-}
+const Home = ({url, author, title, subtitle, node}:IndexProps) => {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>{title}</title>
+          <meta name="description" content={subtitle} />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.main}>
+          {getPostComponent(node)}
+        </main>
+      </div>
+    )
+  }
 interface SiteProps extends Partial<DataFeedContent['siteInfo']> {}
 interface IndexProps  {
     url:string,
+    title:string,
+    subtitle:string,
+    node:PodNode,
     author: SiteProps['author'],
 }
 
 export async function getStaticProps():Promise<{ props :IndexProps}> {
     writeRss()
     generateSitemap()
-    const { author, url  }:IndexProps = getData().siteInfo
-    return {props:{author, url}}
+    const { author, url, title , subtitle , node }:IndexProps = getData().siteInfo
+    return {props:{author, url, title, subtitle, node }}
     
 }
 export default Home
