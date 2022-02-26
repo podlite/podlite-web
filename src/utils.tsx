11f9de4792716210ede68dbe4dd4d8be211f8c12
Aliaksandr Zahatski <zag@cpan.org>
2022-02-26 19:55:40 +0300

    add types

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { PodNode, Rules } from "@podlite/schema"
-import ReactDOMServer from 'react-dom/server'
+import ReactDOMServer from "react-dom/server"
 import Podlite from "@podlite/to-jsx"
 import d from "../built/data.json"
 import * as img from "../built/images"
@@ -30,6 +30,23 @@ export type DataFeed = {
     [path: string]: string
   }
 }
+export type SiteInfo = {
+  postsPerPage: 4
+  favicon: "./favicon.png"
+  url: "http://example.com"
+  pathPrefix: "/"
+  title: "Example"
+  subtitle: "Example"
+  globalStyles?: string
+  node: PodNode
+  author: {
+    name: "Your name here"
+    photo: "./photo.jpg"
+    bio: "Keeping this page up to date 💻."
+    [name: string]: string
+  }
+}
+
 export type FeedContent = publishRecord & {
   number?: number
   sxd: string
@@ -42,10 +59,10 @@ export type ContentRecord = Pick<
   "publishUrl" | "title" | "node" | "sources"
 >
 export function getData(): DataFeedContent {
-    //@ts-ignore
+  //@ts-ignore
   return d as DataFeedContent
-//   const data = fs.readFileSync(DATA_PATH, "utf8").toString()
-//   return JSON.parse(data)
+  //   const data = fs.readFileSync(DATA_PATH, "utf8").toString()
+  //   return JSON.parse(data)
 }
 export function pageNames() {
   const data = getData()
@@ -65,71 +82,96 @@ export function mapPathToImage(path: string): string | undefined {
   return data[path]
 }
 
-export function getPostComponent(podNode:PodNode) {
-    const plugins = (makeComponent):Partial<Rules> => { 
-        
-    const mkComponent = (src) => ( writer, processor )=>( node, ctx, interator )=>{
+export function getPostComponent(podNode: PodNode) {
+  const plugins = (makeComponent): Partial<Rules> => {
+    const mkComponent =
+      src => (writer, processor) => (node, ctx, interator) => {
         // check if node.content defined
-        return makeComponent(src, node, 'content' in node ? interator(node.content, { ...ctx}) : [] )
-    }
-    
+        return makeComponent(
+          src,
+          node,
+          "content" in node ? interator(node.content, { ...ctx }) : []
+        )
+      }
+
     return {
-    //@ts-ignore
-    'TITLE': ()=>()=>null,
-    //@ts-ignore
-    'DESCRIPTION': ()=>()=>null,
-    'React': ()=>(node, ctx)=>{
-        const  conf = makeAttrs(node,ctx)
-        const componentName = conf.getFirstValue('component')
+      //process only content nodes
+      root: () => (node, ctx, interator) => {
+        return interator(node.content, { ...ctx })
+      },
+      //process only content nodes
+      pod: () => (node, ctx, interator) => {
+        return interator(node.content, { ...ctx })
+      },
+      //@ts-ignore
+      TITLE: () => () => null,
+      //@ts-ignore
+      DESCRIPTION: () => () => null,
+      React: () => (node, ctx, interator) => {
+        const conf = makeAttrs(node, ctx)
+        const componentName = conf.getFirstValue("component")
         if (components[componentName]) {
-            return makeComponent(components[componentName], {},'eww')
+          return makeComponent(
+            components[componentName],
+            {},
+            interator(node.content, ctx)
+          )
         } else {
-            return <div style={{color: 'red'}} > Not found: <code ><pre>{componentName}</pre></code></div>
+          return (
+            <div style={{ color: "red" }}>
+              {" "}
+              Not found:{" "}
+              <code>
+                <pre>{componentName}</pre>
+              </code>
+            </div>
+          )
         }
+      },
     }
-}}
-    const node = {
-        interator: podNode,
-        errors: {},
-        toString: () => "",
-        valueOf: () => "",
-        indexingTerms: {},
-        annotations: {},
-        defenitions: {},
-      }
-      // wrap all elements and add line link info
-      const wrapFunction = (node: PodNode, children) => {
-        if (typeof node !== "string" && "type" in node && node.type == "image") {
-          const imageName = node.src
-          if (imageName && img[imageName]) {
-            const placeholder = !node.src.endsWith("gif") ? "blur" : "empty"
-            if (node.src.endsWith("mp4")) {
-              return (
-                <div className="video shadow">
-                  {" "}
-                  <video controls>
-                    {" "}
-                    <source src={img[imageName]} type="video/mp4" />{" "}
-                  </video>
-                </div>
-              )
-            } else {
-              return <img className="shadow_DISABLED" src={img[imageName]} />
-            }
-          } else {
-            return (
-              <>
-                <h1>image {imageName} not found</h1>
-              </>
-            )
-          }
-          return children
+  }
+  const node = {
+    interator: podNode,
+    errors: {},
+    toString: () => "",
+    valueOf: () => "",
+    indexingTerms: {},
+    annotations: {},
+    defenitions: {},
+  }
+  // wrap all elements and add line link info
+  const wrapFunction = (node: PodNode, children) => {
+    if (typeof node !== "string" && "type" in node && node.type == "image") {
+      const imageName = node.src
+      if (imageName && img[imageName]) {
+        const placeholder = !node.src.endsWith("gif") ? "blur" : "empty"
+        if (node.src.endsWith("mp4")) {
+          return (
+            <div className="video shadow">
+              {" "}
+              <video controls>
+                {" "}
+                <source src={img[imageName]} type="video/mp4" />{" "}
+              </video>
+            </div>
+          )
         } else {
-          return children
+          return <img className="shadow_DISABLED" src={img[imageName]} />
         }
+      } else {
+        return (
+          <>
+            <h1>image {imageName} not found</h1>
+          </>
+        )
       }
-    return <Podlite plugins = {plugins} tree={node} wrapElement={wrapFunction} />
+      return children
+    } else {
+      return children
+    }
+  }
+  return <Podlite plugins={plugins} tree={node} wrapElement={wrapFunction} />
 }
-export function convertPodNodeToHtml( node:PodNode):string {
-    return ReactDOMServer.renderToString(getPostComponent(node))
+export function convertPodNodeToHtml(node: PodNode): string {
+  return ReactDOMServer.renderToString(getPostComponent(node))
 }
