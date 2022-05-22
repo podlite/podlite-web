6eabdcf7548b41814abb2240a215621afe08c53e
Aliaksandr Zahatski <github.send@zag.ru>
2022-05-22 22:50:00 +0200

    clean debug

--- src/components/service.tsx
+++ src/components/service.tsx
@@ -1,13 +1,12 @@
 import styles from './service.module.css'
-export const TestComponent = ({children})=>{
+export const TestComponent = ({id,children})=>{
    var style = { "--count-columns ": children.length } as React.CSSProperties;
-  return <div className={styles.Conatainer} style={style}>
-      <h1>Got count:{children.length}</h1>
-<div className={styles.row}>
+  return <div id={id} className={styles.Conatainer} style={style}>
+    <div className={styles.row}>
 
-    {children.map((i,c)=>(
-        <div className={styles.column} key={c}>{i}</div>
-    ))}
+        {children.map((i,c)=>(
+            <div className={styles.column} key={c}>{i}</div>
+        ))}
     </div>
 </div>
 }
