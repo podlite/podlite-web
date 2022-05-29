324af2698d1662ff43bd22a06d4950778bed3c2b
Aliaksandr Zahatski <github.send@zag.ru>
2022-05-29 11:41:06 +0200

    contents component

--- src/components/service.tsx
+++ src/components/service.tsx
@@ -10,5 +10,29 @@ export const TestComponent = ({id,children})=>{
     </div>
 </div>
 }
+
+export const Contents = ({locale='en'})=>{
+moment.locale(locale)
+const groupedByYearMonth = getArticlesGroupedByYearMonth()
+
+const res:JSX.Element[] = []
+for ( const  year of  Object.keys(groupedByYearMonth).sort((a,b)=> parseInt(b,10)-parseInt(a,10)) ) {
+    const months:DataFeedContent["all"] = groupedByYearMonth[year]
+    let isYearAlreadyPut = false;
+    for ( const month of Object.keys(months).sort((a,b)=> parseInt(b,10)-parseInt(a,10)) ) {
+        const monthRecord = <>
+        <h3>{ !isYearAlreadyPut && <time>{year}</time> } 
+            <time>{ moment(`${year}-${parseInt(month,10)+1}`).format('MMMM').toUpperCase()}</time>
+        </h3>
+        { months[month].map(({publishUrl,title,pubdate, node })=>(
+            <a href={publishUrl}><p>{title || getTextContentFromNode(node)}</p><hr/><time dateTime={moment(pubdate).format('YYYY-MM-DD')}>{moment(pubdate).format('Do')}</time></a>       
+           ))
+        }
+        </>
+        isYearAlreadyPut=true;
+        res.push(monthRecord)
+    }
+
+}
     
 export default TestComponent 
