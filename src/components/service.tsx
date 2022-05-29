9481c7c1f121dd995c6471b282f49882a63eb789
Aliaksandr Zahatski <zag@cpan.org>
2022-05-29 11:41:43 +0200

    improve components for pages

--- src/components/service.tsx
+++ src/components/service.tsx
@@ -40,5 +40,58 @@ for ( const  year of  Object.keys(groupedByYearMonth).sort((a,b)=> parseInt(b,10
     }
 
 }
+return <div className="details">{res}</div>
+}
+
+export const Article = ({title, node, shortUrl, key,publishUrl, pubdate})=>(
+        <article key={key}>
+        <header><h1>{title}</h1></header>
+        {getPostComponent(node)}
+        <footer><a href={shortUrl}>{shortUrl}</a><Link href={publishUrl}>{moment(pubdate).format('H:mm on YYYY-MM-DD')}</Link></footer>
+        </article>
+)
+
+export const Page = ({title, node, shortUrl, key,publishUrl, pubdate})=>(
+  <>
+    <article key={key}>
+    <header><h1>{title}</h1></header>
+    {getPostComponent(node)}
+    </article>
+        <TestComponent id="nav">
+        <></>
+        { <Link href='/'>🔼</Link> }
+        <></>
+    </TestComponent>
+  </>
+)
+
+export const ArticlesWithNavigation = ({articles, prev, next }:{articles:any[],prev?:any, next?:any})=>{
+  const makeLink = (title, url) => <Link href={url}><a>{title}</a></Link>
+  return (<>
+    { 
+        articles.map(({publishUrl, ...args}:any)=>Article({key:publishUrl, publishUrl, ...args}))
+    }
+    <TestComponent id="nav">
+        { prev && makeLink(prev.title || getTextContentFromNode(prev.node), prev.publishUrl)}
+        { <div className="navigate">{prev && makeLink("←", prev.publishUrl)}<Link href='/'>🔼</Link>{next &&makeLink("→", next.publishUrl)}</div>}
+        { next && makeLink(next.title || getTextContentFromNode(next.node), next.publishUrl)}
+    </TestComponent>
+  </>
+)}
+
+
+export const LastArticles = ({count=1, id,children})=>{
+    const source =()=>contentData().filter(({type=''}:any)=>type !== 'page')
+    const  articles = source().reverse().slice(0,count)
+    const lastArticleUrl = articles[articles.length-1].publishUrl
+    const articleIndex =source().findIndex(({publishUrl})=>publishUrl===lastArticleUrl)
+    const prev = source()[articleIndex - 1]
+    return (
+        <>
+         <ArticlesWithNavigation articles={articles} prev={prev}/>
+        </>
+    )
+ }
+ 
     
-export default TestComponent 
+export default TestComponent
