27eec7a2664a48fc2f21c52ca1410c82fda42c10
Aliaksandr Zahatski <zag@cpan.org>
2022-06-12 11:43:12 +0200

    add default headers for each page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,38 @@
 import "../styles/pod6.css"
 import "../../built/styles.css"
+import Head from "next/head"
 import type { AppProps } from "next/app"
+import { contentData, getSiteInfo } from "../utils"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const {slug} = pageProps
+    const checkSlug = (slug) => ({ publishUrl }) => {
+        const url = "/" + slug.join("/")
+        return publishUrl.match(url)
+      }
+    const item: any = contentData().find(checkSlug(slug))
+    const {title:siteTitle } = getSiteInfo()
+    const title = `${item?.title} ${siteTitle}` 
+  return <>
+  <Head>
+  <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="/rss.xml"
+        />
+        <meta name="description" content={title} />
+        <meta property="og:site_name" content={siteTitle} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={title} />
+        {/* 
+        TODO:
+        <meta property="og:image" content={metaImageUrl} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={metaImageUrl} /> */}
+  </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
