e8a1741ab64f2c93dba1c119b7952a3b7e2aec64
Aliaksandr Zahatski <github.send@zag.ru>
2022-06-15 08:40:55 +0200

    add viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,6 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:site_name" content={siteTitle} />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         {/* 
         TODO:
         <meta property="og:image" content={metaImageUrl} />
