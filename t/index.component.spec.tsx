5bb4dcc4a7d40ca7e06bd91544c41cfb0efb7986
Aliaksandr Zahatski <zag@cpan.org>
2024-08-11 16:49:52 +0200

    cleanup tests

--- t/index.component.spec.tsx
+++ t/index.component.spec.tsx
@@ -2,11 +2,10 @@ import makeInterator from '@podlite/schema/lib/ast-inerator'
 import { addUrl, publishRecord } from '../src/shared'
 import { convertFileLinksToUrl, parseFiles } from '../src/node-utils'
 import { getTextContentFromNode } from '@podlite/schema'
-import { separateFilesByPlugin } from 'src/plugins'
 
 it('parseFiles', () => {
   const arr = parseFiles('./t/test-blog.pub/*')
-//   console.log(JSON.stringify(arr, null, 2))
+  //   console.log(JSON.stringify(arr, null, 2))
   //   expect(arr.length).toEqual(2)
   // console.log(parseToHtml(nodes));
   // expect(parseToHtml(pod)).toMatchInlineSnapshot(
@@ -31,122 +30,5 @@ it('collect links', () => {
     Array [
       "/2023/4/1/2",
     ]
-  `)
-})
-
-// lets test pocess files depeends in their location
-it('process based on selectings', () => {
-  const config = {
-    plugins: [
-      {
-        name: 'blog.pub',
-        includePatterns: 'dir1/',
-      },
-      {
-        name: 'default',
-        excludePatterns: '.*', // reject all files by default
-      },
-    ],
-  }
-
-  const allFiles = ['t/dir1/index.component.spec.tsx', 't/test-linking/dst.pod6', 't/test-linking/src.pod6']
-  const results = separateFilesByPlugin(allFiles, config)
-  expect(results).toMatchInlineSnapshot(`
-    Object {
-      "blog.pub": Array [
-        "t/dir1/index.component.spec.tsx",
-      ],
-      "default": Array [],
-      "rejected": Array [
-        "t/test-linking/dst.pod6",
-        "t/test-linking/src.pod6",
-      ],
-    }
-  `)
-})
-
-// lets test pocess files depeends in their location
-it('collect rejected', () => {
-  const config = {
-    plugins: [
-      {
-        name: 'blog.pub',
-        includePatterns: 'dir1/',
-      },
-      {
-        name: 'blog.pub',
-        includePatterns: 'dir2/',
-      },
-      {
-        name: 'default',
-        excludePatterns: '.*', // reject all files by default
-      },
-    ],
-  }
-
-  const allFiles = [
-    't/dir1/index.component.spec.tsx',
-    't/dir2/mod1/test-linking/dst.pod6',
-    't/dir2/mod3/test-linking/dst.pod6',
-    't/dir3/mod3/test-linking/dst.pod6',
-    'index.pod6',
-  ]
-  const results = separateFilesByPlugin(allFiles, config)
-  expect(results).toMatchInlineSnapshot(`
-    Object {
-      "blog.pub": Array [
-        "t/dir1/index.component.spec.tsx",
-        "t/dir2/mod1/test-linking/dst.pod6",
-        "t/dir2/mod3/test-linking/dst.pod6",
-      ],
-      "default": Array [],
-      "rejected": Array [
-        "t/dir3/mod3/test-linking/dst.pod6",
-        "index.pod6",
-      ],
-    }
-  `)
-})
-
-it('collect rejected', () => {
-  const config = {
-    plugins: [
-      {
-        name: 'blog.pub',
-        includePatterns: 'dir1/',
-      },
-      {
-        name: 'blog.pub',
-        includePatterns: 'dir2/',
-        excludePatterns: 'dir2/mod3',
-      },
-      {
-        name: 'default',
-        excludePatterns: '.*', // reject all files by default
-      },
-    ],
-  }
-
-  const allFiles = [
-    't/dir1/index.component.spec.tsx',
-    't/dir2/mod1/test-linking/dst.pod6',
-    't/dir2/mod3/test-linking/dst.pod6',
-    't/dir3/mod3/test-linking/dst.pod6',
-    'index.pod6',
-  ]
-  const results = separateFilesByPlugin(allFiles, config)
-  expect(results).toMatchInlineSnapshot(`
-    Object {
-      "blog.pub": Array [
-        "t/dir1/index.component.spec.tsx",
-        "t/dir2/mod1/test-linking/dst.pod6",
-      ],
-      "default": Array [],
-      "rejected": Array [
-        "t/dir2/mod3/test-linking/dst.pod6",
-        "t/dir3/mod3/test-linking/dst.pod6",
-        "index.pod6",
-      ],
-    }
   `)
 })
