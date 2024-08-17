97fd02dc14f497bbca175f310b79a28340e931a1
Aliaksandr Zahatski <zag@cpan.org>
2024-08-17 14:52:10 +0200

    adjust types

--- src/utils/sitemap.ts
+++ src/utils/sitemap.ts
@@ -16,7 +16,7 @@ export function generateSitemap() {
         </url>`
   }
 
-  const pages = contentData()
+  const pages = contentData() as ContentRecord[]
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages.map(addPage).join('\n')}
