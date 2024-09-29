218f3923e8d9afe36bb70db7275fec86714778db
Aliaksandr Zahatski <zag@cpan.org>
2024-09-29 16:27:36 +0200

    store all docs in separeate file

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@ export const INDEX_PATH = process.env.INDEX_PATH || 'index.pod6'
 
 export const BUILT_PATH = path.join(process.cwd(), '/built')
 export const DATA_PATH = path.join(process.cwd(), '/built/data.json')
+export const PAGES_PATH = path.join(process.cwd(), '/built/pages.json')
 export const IMAGE_LIB = path.join(process.cwd(), '/built/images.ts')
 export const COMPONENTS_LIB = path.join(process.cwd(), '/built/components.ts')
 export const STYLES_LIB = path.join(process.cwd(), '/built/styles.css')
