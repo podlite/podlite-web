8055d7a04a1bbc89cf4665925b533b68d767b333
Alexander Zahatski <zag@cpan.org>
2025-10-01 08:14:40 +0200

    fix docker image

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$1" = "bash" ] || [ "$1" = "sh" ] || [ "$1" = "/bin/bash" ] || [ "$1" = "/bin/sh" ]; then
+if [ "$1" = "bash" ] || [ "$1" = "sh" ] || [ "$1" = "/bin/bash" ] || [ "$1" = "/bin/sh" ];
 then
     exec "$@"
 else
